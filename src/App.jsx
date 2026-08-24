81c795d4136384f2d805e326cffdfc2b19ccfac0
Rox Sabil <roxsabil25@gmail.com>
2026-08-24 20:20:49 +0600

    index update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/header/header.jsx";
 import Home from "./components/home/home.jsx";
 import NewsDetails from "./NewsDetails.jsx";
@@ -10,12 +10,23 @@ import Editorials from "./components/editorial/EditorialSection.jsx";
 import AllFeatures from "./components/feature/AllFeatures.jsx";
 import AllNews from "./components/news/AllNews.jsx";
 
+// Scroll to top helper component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const AboutUs = () => <div className="p-8 text-center text-xl font-bold">About Us Page</div>;
 
 function App() {
   return (
     <div className="min-h-screen bg-[#FAF8F5]">
+      <ScrollToTop />
       <Header />
       <main>
         <Routes>
@@ -27,7 +38,6 @@ function App() {
           <Route path="/opinion" element={<AllOpinions />} />
           <Route path="/all-news" element={<AllNews />} />
           <Route path="/feature" element={<AllFeatures />} />
-      
           <Route path="/about-us" element={<AboutUs />} />
         </Routes>
       </main>
